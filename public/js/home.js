440db3c7c43a60b7dc3404fcadaef65db82e3427
Brendan Lahey <lahey1991@gmail.com>
2025-01-01 10:39:12 -0600

    WIP: home page

--- public/js/home.js
+++ public/js/home.js
@@ -2,13 +2,13 @@ import { Table } from "../../private/js/class/Table.js";
 import { Element } from "../../private/js/class/Element.js";
 import { Button } from "../../private/js/class/Button.js";
 
-const toolbar = new Table('applications');
+const table = new Table('applications');
 
 const all = new Button('all-btn', () => location.reload(true));
-const sent = new Button('sent-btn', () => toolbar.filterTable(collectionData, 0));
-const seen = new Button('seen-btn', () => toolbar.filterTable(collectionData, 1));
-const interviewed = new Button('interviewed-btn', () => toolbar.filterTable(collectionData, 2));
-const position = new Button('position-btn', () => toolbar.filterTable(collectionData, 3));
+const sent = new Button('sent-btn', () => table.filterTable(collectionData, 0));
+const seen = new Button('seen-btn', () => table.filterTable(collectionData, 1));
+const interviewed = new Button('interviewed-btn', () => table.filterTable(collectionData, 2));
+const position = new Button('position-btn', () => table.filterTable(collectionData, 3));
 const results = new Element('results');
 
 
@@ -16,12 +16,14 @@ const searchBar = document.getElementById('search');
 searchBar.addEventListener('change',(e) => applicationSearch(e.target.value))
 
 function applicationSearch(needle){
-        results.clearChildren();
-        console.log('cleared')
-        const filteredResults = collectionData.filter((app) => app.position.startsWith(needle))
-        filteredResults.forEach((i) => {
-            let li = document.createElement('li');
-            li.appendChild(document.createTextNode(i.position))
-            results.insert(li)
-        })
+        if(needle){
+            results.clearChildren();
+            console.log(needle)
+            const filteredResults = collectionData.filter((app) => app.position.startsWith(needle))
+            filteredResults.forEach((i) => {
+                let li = document.createElement('li');
+                li.appendChild(document.createTextNode(i.position))
+                results.insert(li)
+            })
+        }
 }
